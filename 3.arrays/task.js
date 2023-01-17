e06e0e79a4b6216e4981f3f63485fe610295328f
umniknik <kaaaaaljan@mail.ru>
2023-01-17 19:19:34 +0300

    3.arrays: задание по занятию массивы - готово

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,13 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every(item => item === arr2[arr1.indexOf(item)]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    return users.filter(item => item.gender === gender).map(item => item.age).reduce((acc, item, index, arr22) => {
+        acc += item;
+        if (index === arr22.length - 1) {
+            return acc / arr22.length;
+        }
+        return acc;
+    }, 0);
 }
