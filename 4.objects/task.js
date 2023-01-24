47ab2e304791628dcfe140dee077489013464f6e
umniknik <kaaaaaljan@mail.ru>
2023-01-24 19:02:15 +0300

    4.objects: задание по занятию Объекты - готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (this.hasOwnProperty('marks') && !this.hasOwnProperty('excluded')) {
+        this.marks.push(...marks);
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (!this.marks || !this.marks.length) {
+        return 0;
+    } else {
+        return this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    if (reason) {
+        delete this.subject;
+        delete this.marks;
+        this.excluded = reason;
+    }
 }
