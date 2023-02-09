5016d7c258ccce894384e3e1b5e195dcea7dc4c5
umniknik <kaaaaaljan@mail.ru>
2023-02-09 11:52:38 +0300

    7.acync: задание по занятию 'Асинхронность' - готово

---
+++ 7.async/task.js
@@ -0,0 +1,64 @@
+class AlarmClock {
+    constructor(intervalId = null) {
+        this.alarmCollection = [];
+        this.intervalId = intervalId;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error('Отсутствуют обязательные аргументы');
+            // return;
+        }
+
+        this.alarmCollection.forEach(function (item) {
+            if (item.time === time) {
+                console.warn('Уже присутствует звонок на это же время');
+                //return true;
+            }
+        })
+
+        this.alarmCollection.push({
+            callback: callback,
+            time: time,
+            canCall: true
+        })
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(item => item.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        const CurrenTime = new Date();
+        return (`${CurrenTime.getHours()}:${CurrenTime.getMinutes()}`);
+    }
+
+    start() {
+        if (this.intervalId === null) {
+            this.intervalId = setInterval(() => {
+                this.alarmCollection.forEach((item) => {
+                    if (item.time === this.getCurrentFormattedTime()) {
+                        item.canCall = false;
+                        item.callback();
+                    }
+                })
+            }, 1000)
+        } else { console.log('Завершаем программу, т.к. уже есть интервал') }
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach((item) => {
+            item.canCall = true;
+        })
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
