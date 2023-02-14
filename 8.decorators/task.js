54a92b7560fd5d5ae1b13c5ecda3303665b2a1a1
umniknik <kaaaaaljan@mail.ru>
2023-02-14 21:30:24 +0300

    8.decorators: задание по занятию 'Декораторы' - готово

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,50 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+    function wrapper(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((item) => item.hash === hash);
+
+        if (objectInCache) {
+            console.log("Из кэша: " + objectInCache.value);
+            return "Из кэша: " + objectInCache.value;
+        }
+
+        let result = func(...args);
+        cache.push({ hash: hash, value: result });
+
+        if (cache.length > 5) {
+            cache.shift();
+        }
+
+        console.log("Вычисляем: " + result);
+        return "Вычисляем: " + result;
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutID = null;
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+
+    function wrapper(...args) {
+        wrapper.allCount = wrapper.allCount + 1;
+
+        if (timeoutID === null) {
+            func(...args);
+            wrapper.count = wrapper.count + 1;
+        }
+
+        if (timeoutID) {
+            clearTimeout(timeoutID);
+        }
+
+        timeoutID = setTimeout(() => {
+            func(...args);
+            wrapper.count = wrapper.count + 1;
+        }, delay)
+    }
+    return wrapper;
 }
